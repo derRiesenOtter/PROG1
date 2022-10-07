0e9580725de0bacca3dc9ae6537ba5872d447c0c
derRiesenOtter <robin.ender@icloud.com>
2022-10-07 11:59:47 +0200

    x

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main (String[] args) {
+        int number_1 = 7;
+        int number_2 = 4;
+        int sum;
+        sum = number_1 + number_2;
+        System.out.println("Hello "+sum);
+    }
+}
