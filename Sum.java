4fcba65594a2d222d928496c682e51bb9348483f
derRiesenOtter <robin.ender@icloud.com>
2022-10-12 10:23:36 +0200

    vim test

--- Sum.java
+++ Sum.java
@@ -1,9 +1,9 @@
 public class Sum {
     public static void main (String[] args) {
         int number_1 = 7;
-        int number_2 = 4;
+        int number_2 = 14;
         int sum;
         sum = number_1 + number_2;
-        System.out.println("Hello "+sum);
+        System.out.println("What's nine plus ten? "+sum);
     }
 }
